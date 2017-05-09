2f1a7aa2d03b31114652ba3bcc259cec0b505bf1
ItsYaBoiBry <bryanlow987@gmail.com>
2017-05-09 23:37:24 +0800

    -Created new Events Page list(Currently empty and lacking of events). -Added an example image to test the intent over to the next page. image will be updated soon as the portion of the code is done

--- app/src/main/java/android/myapplicationdev/com/webeventv10/MainActivity.java
+++ app/src/main/java/android/myapplicationdev/com/webeventv10/MainActivity.java
@@ -1,8 +1,11 @@
 package android.myapplicationdev.com.webeventv10;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private ImageView iv_Events;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +29,16 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        iv_Events = (ImageView) findViewById(R.id.imageView_eventsPage);
+        iv_Events.setImageResource(R.drawable.sample_wedding_event);
+        iv_Events.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                Intent intentToEventsPage =  new Intent(MainActivity.this, EventsPage.class);
+                startActivity(intentToEventsPage);
             }
         });
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -98,3 +106,4 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
+
